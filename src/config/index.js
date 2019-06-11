d1b35dde8936d4e36b465bb45583960cda3e7009
shruthikakwe <shruthi.kakve@gmail.com>
2019-06-10 20:38:58 -0400

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
-  API_URL: 'https://raw.githubusercontent.com/my3hegde/conference-app/master/',
+  API_URL: 'https://raw.githubusercontent.com/shruthikakwe/HAA/master/',
   PRIMARY_BG_COLOR: '#353535',
   PRIMARY_TEXT_COLOR: '#999999',
   SECONDARY_BG_COLOR: '#F5FCFF',
